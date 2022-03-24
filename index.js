cd27203eca1248038ef08578b71d0d083c3f1038
Vimal-Yappes <vimal.raj@yappes.com>
2022-03-24 12:36:26 +0530

    t

--- index.js
+++ index.js
@@ -2,6 +2,60 @@ const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
 const port = 3000
+const dotenv = require('dotenv');
+
+// get config vars
+dotenv.config();
+
+// access config var
+process.env.TOKEN_SECRET;
+
+
+function generateAccessToken(username) {
+  return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+}
+
+
+app.post('/api/createNewUser', (req, res) => {
+  // ...
+
+  const token = generateAccessToken({ username: req.body.username });
+  res.json(token);
+
+  // ...
+});
+
+
+const jwt = require('jsonwebtoken');
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    console.log(err)
+
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+
+    next()
+  })
+}
+
+app.get('/api/userOrders', authenticateToken, (req, res) => {
+  // executes after authenticateToken
+  // ...
+})
+
+
+// get token from fetch request
+const token = await res.json();
+
+// set token in cookie
+document.cookie = `token=${token}`
 
 app.use(bodyParser.json())
 app.use(
